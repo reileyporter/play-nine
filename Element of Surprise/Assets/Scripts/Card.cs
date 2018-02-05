7b5b593f282380993dfa77af6a5200c99987ffdf
Justin B <jbaumant@gmail.com>
2018-02-04 20:52:58 -0800

    Proximity system for cards partially implemented

--- Element of Surprise/Assets/Scripts/Card.cs
+++ Element of Surprise/Assets/Scripts/Card.cs
@@ -6,23 +6,29 @@ public class Card : MonoBehaviour {
 
     // UI related Variables
     Vector3 targetPosition; // for following the mouse around
-    Vector3 cardsNormalPosition; // where the card should be, as assigned by the deck or discard pile
-    float smoothTime = .1f;
-    private Vector3 velocity = Vector3.zero;
+    GameObject cardsNormalPosition; // where the card should be, as assigned by the deck or discard pile
+    Vector3 velocity = Vector3.zero; // for the SmoothDamp function
 
     // Logic Related Variables
 
+    // Test Variables
+    SpriteRenderer spriteRenderer;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Test stuff
+        spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (cardsNormalPosition != null)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, cardsNormalPosition.transform.position, ref velocity, /*smoothTime*/ .1f, 1000.0f);
+        }
 	}
 
-    private void OnMouseDrag()
+    void OnMouseDrag()
     {
 		// converts mouse position into a world position based on the camera
 		targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -31,6 +37,6 @@ public class Card : MonoBehaviour {
         // z position needs to still be set properly
         targetPosition.z = -1f;
         // Moves the card to position of the target, and dampens the 
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, 1000.0f);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, /*smoothTime*/ .1f , 1000.0f);
     }
 }
---
+++ Element of Surprise/Assets/Scripts/Proximity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proximity : MonoBehaviour {
+    public List<GameObject> nearbyObjects;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log(coll);
+        if (coll.gameObject.tag == "CardPosition")
+        nearbyObjects.Add(coll.gameObject);
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+	{
+        if (coll.gameObject.tag == "CardPosition")
+        nearbyObjects.Remove(coll.gameObject);
+	}
+
+}

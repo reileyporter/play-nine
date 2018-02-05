8c45217d44e01ee1efaa01a95e684c33a6e2c296
Justin B <jbaumant@gmail.com>
2018-02-04 17:29:12 -0800

    Card Movement

--- Element of Surprise/Assets/Scripts/Card.cs
+++ Element of Surprise/Assets/Scripts/Card.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Card : MonoBehaviour {
 
+    // UI related Variables
+    Vector3 targetPosition; // for following the mouse around
+    Vector3 cardsNormalPosition; // where the card should be, as assigned by the deck or discard pile
+    float smoothTime = .1f;
+    private Vector3 velocity = Vector3.zero;
+
+    // Logic Related Variables
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +21,16 @@ public class Card : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void OnMouseDrag()
+    {
+		// converts mouse position into a world position based on the camera
+		targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // at this point the card would snap to the center of the mouse, so I added the smoothing because of that
+
+        // z position needs to still be set properly
+        targetPosition.z = -1f;
+        // Moves the card to position of the target, and dampens the 
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, 1000.0f);
+    }
 }
